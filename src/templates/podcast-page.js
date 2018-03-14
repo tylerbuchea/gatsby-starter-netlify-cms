016e0976132233961eb5930c185c2a8e3d4fba42
Tyler Buchea <tyler@buchea.com>
2018-03-14 11:19:16 -0700

    Add podcast collection

---
+++ src/templates/podcast-page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Features from '../components/Features'
+import Testimonials from '../components/Testimonials'
+import Pricing from '../components/Pricing'
+
+export const PodcastPageTemplate = (props) => {
+  return (
+    <h1>Hi {JSON.stringify(props)}</h1>
+  );
+}
